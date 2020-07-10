dd3213f202c9b662d7097c827a40a3e0acd46c7f
kwong <kjwxfn@qq.com>
2020-07-10 14:45:36 +0800

    新增微博爬虫

--- scrapy_start.py
+++ scrapy_start.py
@@ -1,4 +1,6 @@
 import os
+import random
+import time
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
@@ -7,6 +9,8 @@ if __name__ == "__main__":
         os.system(r"nohup scrapy crawl lianjia_spider > /usr/local/project/python/spider/log/lianjia-`date +%y-%m-%d`.log 2>&1 &")
 
     def start_weibo_spider():
+        s = round(random.uniform(1, 1.8), 2)
+        time.sleep(s)
         os.system(
             r"nohup scrapy crawl weibo_spider > /usr/local/project/python/spider/log/weibo-`date +%y-%m-%d`.log 2>&1 &")
 
